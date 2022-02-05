567b70aacdf974b49506bd34b4cda43e55c6e995
jmueller187 <92120094+jmueller187@users.noreply.github.com>
2022-02-04 21:54:55 -0500

    Resources added for Deliverable 3.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,3 +19,17 @@ total_summary <- SuspensionCoil_table %>%  summarize(Mean=mean(PSI), Median=medi
 # Create Lot summary data frame for mean, median, variance and standard deviation of PSI column
 lot_summary <- SuspensionCoil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 
+# Create t-test to test PSI across all manufacturing lots
+t.test((SuspensionCoil_table$PSI),mu=(total_summary$Median))
+
+# Create t-test to test PSI across Lot 1
+lot1 <- subset(SuspensionCoil_table, Manufacturing_Lot == "Lot1")
+t.test((lot1$PSI),mu=(total_summary$Median))
+
+# Create t-test to test PSI across Lot 2
+lot2 <- subset(SuspensionCoil_table, Manufacturing_Lot == "Lot2")
+t.test((lot2$PSI),mu=(total_summary$Median))
+
+# Create t-test to test PSI across Lot 3
+lot3 <- subset(SuspensionCoil_table, Manufacturing_Lot == "Lot3")
+t.test((lot3$PSI),mu=(total_summary$Median))
