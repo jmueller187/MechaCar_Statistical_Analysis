1184819d096da492f68cbc053450229e10fa9a8b
jmueller187 <92120094+jmueller187@users.noreply.github.com>
2022-02-04 19:58:38 -0500

    Resources added for Deliverable 2.

--- MechCarChallenge.R
+++ MechCarChallenge.R
@@ -1,7 +1,7 @@
 # Load dplyr package
 library(dplyr)
 
-# Import & read csv file
+# Import & read MechCar_mpg csv file
 MechaCar_table <- read.csv(file = 'MechaCar_mpg.csv')
 
 # Create multiple linear regression model
@@ -9,3 +9,13 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Retrieve statistical metrics from multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_table))
+
+# Import & read SuspensionCoil csv file
+SuspensionCoil_table <- read.csv(file = 'Suspension_Coil.csv')
+
+# Create total_summary data frame for mean, median, variance and standard deviation of PSI column
+total_summary <- SuspensionCoil_table %>%  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+# Create Lot summary data frame for mean, median, variance and standard deviation of PSI column
+lot_summary <- SuspensionCoil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
